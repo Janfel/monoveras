1e6c6f5600755a6c6e387d5e22155b4fbf3a95ea
Jan Felix Langenbach <o.hase3@gmail.com>
2018-09-06 17:43:17 +0200

    Travis now double deploying binaries?

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
-java -jar bob.jar --archive --platform "$PLATFORM" --bundle-output "build/release/" resolve distclean build bundle
+platforms="x86-win32 x86-linux"
+
+mkdir -p build/release
+java -jar bob.jar --archive clean resolve build
+
+for platform in $platforms
+do
+    mkdir "build/release/$platform"
+    java -jar bob.jar --bundle-output "build/release/$platform" --platform "$platform" bundle
+done
 
