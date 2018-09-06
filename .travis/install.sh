9526a382326fe6c542026d8a933d5db40a791a61
Jan Felix Langenbach <o.hase3@gmail.com>
2018-09-06 15:19:04 +0200

    Travis second try

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,4 +2,4 @@
 
 versionstr=1b90c9a905d634b766b467e3536458b9210ec812
 
-wget -cO bob.jar "http://d.defold.com/archive/$versionstr/bob/bob.jar"
+wget -nv -cO bob.jar "http://d.defold.com/archive/$versionstr/bob/bob.jar"
