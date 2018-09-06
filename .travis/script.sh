57c51f6a0a262ff495730c85f2936278542ce74a
Jan Felix Langenbach <o.hase3@gmail.com>
2018-09-06 18:06:22 +0200

    Fix bob array iteration

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-platforms="x86-win32 x86-linux"
+platforms=(x86-win32 x86-linux)
 
 mkdir -p build/release
 java -jar bob.jar --archive clean resolve build
 
-for platform in $platforms
+for platform in "${platforms[@]}"
 do
     mkdir "build/release/$platform"
     java -jar bob.jar --bundle-output "build/release/$platform" --platform "$platform" bundle
