358cf60fc7eca5b5417701da50eb511347b3eb94
Jan Felix Langenbach <o.hase3@gmail.com>
2018-09-06 19:11:03 +0200

    Fix naming and permission issues

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
-platforms=(x86-win32 x86-linux)
+platforms=(Windows Linux)
+declare -A name=(
+  [Windows]=x86-win32
+  [Linux]=x86-linux
+  [macOS]=x86-darwin
+  [macOS64]=x86_64-darwin
+  [HTML5]=js-web
+  [iOS]=armv7-darwin
+  [Android]=armv7-android
+)
 
 mkdir -p build/release
-java -jar bob.jar --archive clean resolve build
+java -jar bob.jar --archive distclean resolve build
 
 for platform in "${platforms[@]}"
 do
     mkdir "build/release/$platform"
-    java -jar bob.jar --bundle-output "build/release/$platform" --platform "$platform" bundle
-    tar -czf "build/release/$platform.tar.gz" "build/release/$platform"
+    java -jar bob.jar --bundle-output "build/release/$platform" --platform "${name[$platform]}" bundle
+    tar --no-same-owner --no-same-permissions -C "build/release" -czf "build/release/$platform.tar.gz" "$platform"
 done
 
