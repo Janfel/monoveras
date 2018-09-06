a4dc64e3af271bcc053f8b9369871ed57d1cd8fb
Jan Felix Langenbach <o.hase3@gmail.com>
2018-09-06 15:10:29 +0200

    Travis Support

---
+++ .travis/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+versionstr=1b90c9a905d634b766b467e3536458b9210ec812
+
+wget -cO bob.jar "http://d.defold.com/archive/$versionstr/bob/bob.jar"
---
+++ .travis/script.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+java -jar bob.jar --archive --platform "$PLATFORM" resolve distclean build bundle
+
