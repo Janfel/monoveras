b5481f6cb00665de3e3d86f48caf8025efa8f284
Jan Felix Langenbach <o.hase3@gmail.com>
2018-11-26 20:45:12 +0100

    Add hero.go

---
+++ main/hero/hero.go
@@ -0,0 +1,21 @@
+embedded_components {
+  id: "spinemodel"
+  type: "spinemodel"
+  data: "spine_scene: \"/main/hero/hero.spinescene\"\n"
+  "default_animation: \"run_right\"\n"
+  "skin: \"\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "material: \"/builtins/materials/spine.material\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
