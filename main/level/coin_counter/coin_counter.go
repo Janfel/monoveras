50b86dee7e4d77f447b7f004023f205f2f5378e6
Unknown <o.hase3@gmail.com>
2018-12-12 20:58:00 +0100

    Add Coin Counter

---
+++ main/level/coin_counter/coin_counter.go
@@ -0,0 +1,15 @@
+components {
+  id: "coin_counter"
+  component: "/main/level/coin_counter/coin_counter.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
